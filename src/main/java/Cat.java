af7ca914525cc55c3658b8a702a454b6fefabf0b
Aaahooly <andreygluh@yandex.ru>
2022-09-16 14:18:18 +0300

    Добавлены поля + Сетеры и гетеры в Класс Cat

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,2 +1,20 @@
 public class Cat {
+    private String nickName;
+    private int lives;
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public int getLives() {
+        return lives;
+    }
+
+    public void setLives(int lives) {
+        this.lives = lives;
+    }
 }
